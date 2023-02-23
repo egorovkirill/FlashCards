ad027fb8e36f7ebaac63de339a3d9f02334b599f
Kirill <126064091+egorovkirill@users.noreply.github.com>
2023-02-23 13:24:47 +0300

    добавлена генерация изображения по реквесту
    1. Клиент теперь имеет возможность получить сгенерированное DALL-E изображение по текстовому запросу в REST API

--- imageGeneration/cmd/main.go
+++ imageGeneration/cmd/main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/Shopify/sarama"
+	"io"
 	"log"
+	"net/http"
 )
+
+type Card struct {
+	Front string `json:"front"`
+}
+
+type Response struct {
+	Data []struct {
+		URL string `json:"url"`
+	} `json:"data"`
+}
+
+type Request struct {
+	Prompt         string `json:"prompt"`
+	NumImages      int    `json:"num_images"`
+	Size           string `json:"size"`
+	ResponseFormat string `json:"response_format"`
+}
+
+var reqHeaders = map[string]string{
+	"Content-Type":  "application/json",
+	"Authorization": "Bearer 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",
+}
+
+var apiURL = "https://api.openai.com/v1/images/generations"
 
 func main() {
 	config := sarama.NewConfig()
@@ -37,10 +65,55 @@ func main() {
 		select {
 		case msg := <-partitionConsumer.Messages():
 			// Replace this with your actual processing logic
+			var input Card
 			fmt.Println("Received message: ", string(msg.Value))
+			err := json.Unmarshal([]byte(string(msg.Value)), &input)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println(GenerateImage(input.Front))
 		case err := <-partitionConsumer.Errors():
+			fmt.Println("222222o")
 			log.Println("Error: ", err)
 			return
 		}
 	}
+}
+
+func GenerateImage(prompt string) (string, error) {
+	request := Request{
+		Prompt:         prompt,
+		NumImages:      1,
+		Size:           "256x256",
+		ResponseFormat: "url",
+	}
+
+	payload, err := json.Marshal(request)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(payload))
+	if err != nil {
+		return "", err
+	}
+
+	for key, value := range reqHeaders {
+		req.Header.Set(key, value)
+	}
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	var response Response
+	if err := json.Unmarshal(body, &response); err != nil {
+		return "", err
+	}
+	return response.Data[0].URL, nil
 }
